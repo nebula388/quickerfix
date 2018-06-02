quickfix/quickfix

Work in progress - cmake stuff.

examples/executor/C++/executor.cpp
@@ -23,14 +23,14 @@
 #include "config.h"
 #endif
 
-#include "quickfix/FileStore.h"
-#include "quickfix/SocketAcceptor.h"
+#include "FileStore.h"
+#include "SocketAcceptor.h"
 #ifdef HAVE_SSL
-#include "quickfix/ThreadedSSLSocketAcceptor.h"
-#include "quickfix/SSLSocketAcceptor.h"
+#include "ThreadedSSLSocketAcceptor.h"
+#include "SSLSocketAcceptor.h"
 #endif
-#include "quickfix/Log.h"
-#include "quickfix/SessionSettings.h"
+#include "Log.h"
+#include "SessionSettings.h"
 #include "Application.h"
 #include <string>
 #include <iostream>

nebula388/quickerfix

examples/executor/C++/executor.cpp
@@ -23,15 +23,15 @@
 #include "config.h"
 #endif
 
-#include "quickfix/FileStore.h"
-#include "quickfix/NullStore.h"
-#include "quickfix/ThreadedSocketAcceptor.h"
+#include "NullStore.h"
+#include "FileStore.h"
+#include "ThreadedSocketAcceptor.h"
 #ifdef HAVE_SSL
-#include "quickfix/ThreadedSSLSocketAcceptor.h"
-#include "quickfix/SSLSocketAcceptor.h"
+#include "ThreadedSSLSocketAcceptor.h"
+#include "SSLSocketAcceptor.h"
 #endif
-#include "quickfix/Log.h"
-#include "quickfix/SessionSettings.h"
+#include "Log.h"
+#include "SessionSettings.h"
 #include "Application.h"
 #include <string>
 #include <iostream>