c6df149d1340c8bf67924c15bae64377dbd2717b
Ke Liu <ke.liu@nebulachamp.com>
2019-11-04 02:36:42 +0800

    fix cmake

--- my_cmake.sh
+++ my_cmake.sh
@@ -2,3 +2,6 @@ mkdir cmake_files
 cd cmake_files
 cmake  -DCMAKE_BUILD_TYPE=RELEASE -DHAVE_TBB=ON -DWITH_ALLOCATOR=TBB -DTBB_LIBRARIES_RELEASE=/usr/lib/x86_64-linux-gnu/libtbb.so -DCMAKE_CXX_COMPILER=/usr/bin/g++-7 ../
 make quickfix
+cd ..
+mkdir release
+cp cmake_files/src/C++/libquickfix.a ./release/
