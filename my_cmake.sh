944d4c13159ef001d3ea5986205fbdddf473c857
Ke Liu <ke.liu@nebulachamp.com>
2019-11-04 17:45:35 +0800

    enable flat field map

--- my_cmake.sh
+++ my_cmake.sh
@@ -1,7 +1,7 @@
 mkdir cmake_files
 cd cmake_files
-cmake  -DCMAKE_BUILD_TYPE=RELEASE -DHAVE_TBB=ON -DWITH_ALLOCATOR=TBB -DTBB_LIBRARIES_RELEASE=/usr/lib/x86_64-linux-gnu/libtbb.so -DCMAKE_CXX_COMPILER=/usr/bin/g++-7 ../
-make quickfix
+cmake  -DCMAKE_BUILD_TYPE=RELEASE -DHAVE_TBB=ON -DWITH_ALLOCATOR=TBB -DTBB_LIBRARIES_RELEASE=/usr/lib/x86_64-linux-gnu/libtbb.so -DCMAKE_CXX_COMPILER=/usr/bin/g++-7 -DENABLE_FLAT_FIELDMAP=Y ../
+make
 cd ..
 mkdir release
 cp cmake_files/src/C++/libquickfix.a ./release/
